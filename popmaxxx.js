90d23c5b466753fc8744c43db02dad3e4b47d93b
strodong <85514413+strodong@users.noreply.github.com>
2021-07-08 20:19:57 +0700

    Update popmaxxx.js

--- popmaxxx.js
+++ popmaxxx.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
     console.log('jquery ready!');
 
-	var popbox = `<div class="popbox hide" id="popbox"><div aria-label='Close' class="pop-overlay" role="button" tabindex="0"/><div class="pop-content"><div class="popcontent" align="center"> <img src="https://1.bp.blogspot.com/-1yKEkUQEFSQ/YObC49aqaRI/AAAAAAAAA-0/3f0ZkXvEp7EGlpxMtF-KkL0n0vZ61sKygCLcBGAsYHQ/s16000/Lifepoints%2BBanner%25282%2529.jpg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" width="336" height="280" alt="" /> Pendaftaran hanya untuk di bulan July, kuy sebelum di tutup pendaftarannya. Kapan lagi bisa nyari uang gampang <b>tinggal daftar aja tanpa BIAYA alias Gratis</b>.<br> <br><button class='g_url btn btn-success btn-dwn m-2'>Oke saya mau !!!</button></div> <button class='g_url popbox-close-button'>&times;</button></div></div>`;
+	var popbox = `<div class="popbox hide" id="popbox"><div aria-label='Close' class="pop-overlay" role="button" tabindex="0"/><div class="pop-content"><div class="popcontent" align="center"> <img src="https://1.bp.blogspot.com/-v3C9Oq3LZ68/YOb7CBBNBUI/AAAAAAAAA_E/N34o_vUPxkwupMpwmLFMx8i7spfl-8cngCLcBGAsYHQ/s16000/Copy%2Bof%2BBanner%2B2.jpg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" width="336" height="280" alt="" /> Pendaftaran hanya untuk di bulan July, yuk sebelum di tutup pendaftarannya. Kapan lagi bisa nyari uang gampang hasil jutaan <b>tinggal daftar aja tanpa BIAYA alias Gratis</b>.<br> <br><button class='g_url btn btn-success btn-dwn m-2'>Oke saya mau !!!</button></div> <button class='g_url popbox-close-button'>&times;</button></div></div>`;
 
 	$(document.body).append(popbox);
 
