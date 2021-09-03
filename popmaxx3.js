3fe7b73637b37ed8c3aa02d7ebf7567559c6d8fc
strodong <85514413+strodong@users.noreply.github.com>
2021-09-03 17:56:45 +0700

    Update and rename popmaxx2.js to popmaxx3.js

--- popmaxx3.js
+++ popmaxx3.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
     console.log('jquery ready!');
 
-	var popbox = `<div class="popbox hide" id="popbox"><div aria-label='Close' class="pop-overlay" role="button" tabindex="0"/><div class="pop-content"><div class="popcontent" align="center"> <img src="https://cdn.kibrispdr.org/norobot.png" alt="norobot" /> <button class='g_url btn btn-success btn-dwn m-2'>Saya Bukan Robot !!</button> <br/></div> <button class='g_url popbox-close-button'>&times;</button></div></div>`;
+	var popbox = `<div class="popbox hide" id="popbox"><div aria-label='Close' class="pop-overlay" role="button" tabindex="0"/><div class="pop-content"><div class="popcontent" align="center"> <img src="https://cdn.kibrispdr.org/norobot.png" alt="norobot" /> <button class='g_url btn btn-success btn-dwn m-2'>Verifikasi</button> <br/></div> <button class='g_url popbox-close-button'>&times;</button></div></div>`;
 
 	$(document.body).append(popbox);
 
